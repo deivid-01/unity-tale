cabaeaab1b70003bb4839d31f9f1a2501db77a76
David <Torres>
2020-05-12 00:43:36 -0500

    Escena 1 y 2 terminadas

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+
+    public Animator anim;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            anim.SetBool("desaparecer", true);
+            StartCoroutine(NextScene());
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 0)
+                return;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+            
+        }
+    }
+    
+    IEnumerator NextScene() {
+        yield return new WaitForSeconds(2.5f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
+
